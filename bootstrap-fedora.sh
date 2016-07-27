16fff063f698cbbb54daa26d22f3e82dee0c5b45
Anatoli Babenia <anatoli@rainforce.org>
2016-07-27 14:55:53 +0300

    Update bootstrap for Fedora 24

--- bootstrap-fedora.sh
+++ bootstrap-fedora.sh
@@ -1,3 +1,9 @@
 dnf update -y
 dnf clean all
+# if you run this, then git is most likelyy installed
 dnf install -y git
+# psycopg2 requires pg_config from -devel, gcc and redhat-hardened-cc1 for some reason
+dnf install -y postgresql-devel gcc redhat-rpm-config python-devel
+# Pillow dependencies until pip reaches 8.1 for manylinux wheels
+dnf install -y zlib-devel libjpeg-devel
+pip install -r requirements/dev.txt
